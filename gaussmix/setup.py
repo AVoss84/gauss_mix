2e2c8a680d48b0306b399f5622c8058fb76f8735
AVoss84 <a.vosseler@gmx.net>
2020-02-06 22:57:06 +0100

    added new readme

---
+++ gaussmix/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gaussmix',
+    version='0.0.1',
+    description='Gaussian mixture model',
+    #url='https://github.com/AVoss84/gauss_mix',
+    author='Alexander Vosseler',
+    #keywords='rest restful api flask swagger openapi flask-restplus',
+    packages = find_packages()
+    #install_requires=['flask-restplus==0.9.2', 'Flask-SQLAlchemy==2.1']
+)
+
+
